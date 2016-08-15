51ee7ed5273200534f66f88bdc3f6948795584aa
vathsav <vathsav@live.in>
2016-08-16 01:49:34 +0530

    Removed a bunch of unnecessary files from Git.

--- app/src/main/java/com/vathsav/lynk/SplashActivity.java
+++ app/src/main/java/com/vathsav/lynk/SplashActivity.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.vathsav.lynk.utils.Utils;
@@ -29,47 +27,41 @@ public class SplashActivity extends AppCompatActivity {
         ParticleCloudSDK.init(getApplicationContext());
         Utils.sparkCloud = ParticleCloudSDK.getCloud();
 
-        Button buttonSplashLogin = (Button) findViewById(R.id.button_splash_login);
-        buttonSplashLogin.setOnClickListener(new View.OnClickListener() {
+        loginTask = Async.executeAsync(Utils.sparkCloud, new Async.ApiWork<ParticleCloud, Void>() {
             @Override
-            public void onClick(View view) {
-                loginTask = Async.executeAsync(Utils.sparkCloud, new Async.ApiWork<ParticleCloud, Void>() {
-                    @Override
-                    public Void callApi(ParticleCloud particleCloud) throws ParticleCloudException, IOException {
-                        particleCloud.logIn("email@domain.com", "password");
+            public Void callApi(ParticleCloud particleCloud) throws ParticleCloudException, IOException {
+                particleCloud.logIn("email@domain.com", "password");
 
-                        if (particleCloud.getDevices().contains(particleCloud.getDevice("ruthless_dynamite"))) {
-                            Utils.ruthlessDynamite = particleCloud.getDevice("ruthless_dynamite");
-                            Utils.deviceId = Utils.ruthlessDynamite.getID();
-                            Log.v("Device id:", Utils.deviceId);
-                        } else {
-                            Toast.makeText(getApplicationContext(), "Unable to find Core.", Toast.LENGTH_SHORT).show();
-                        }
+                if (particleCloud.getDevices().contains(particleCloud.getDevice("ruthless_dynamite"))) {
+                    Utils.ruthlessDynamite = particleCloud.getDevice("ruthless_dynamite");
+                    Utils.deviceId = Utils.ruthlessDynamite.getID();
+                    Log.v("Device id:", Utils.deviceId);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Unable to find Core.", Toast.LENGTH_SHORT).show();
+                }
 
-                        return null;
-                    }
+                return null;
+            }
 
-                    @Override
-                    public void onTaskFinished() {
-                        loginTask = null;
-                    }
+            @Override
+            public void onTaskFinished() {
+                loginTask = null;
+            }
 
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        // TODO: 15/08/16 Handle token expiration D/ParticleAccessToken: Scheduling token expiration for Sun Nov 13 19:23:38 GMT+05:30 2016 (7776000000ms.
-                        if (isFinishing()) {
-                            return;
-                        }
+            @Override
+            public void onSuccess(Void aVoid) {
+                // TODO: 15/08/16 Handle token expiration D/ParticleAccessToken: Scheduling token expiration for Sun Nov 13 19:23:38 GMT+05:30 2016 (7776000000ms.
+                if (isFinishing()) {
+                    return;
+                }
 
-                        Intent openMainActivity = new Intent("com.vathsav.lynk.MAIN");
-                        startActivity(openMainActivity);
-                    }
+                Intent openMainActivity = new Intent("com.vathsav.lynk.MAIN");
+                startActivity(openMainActivity);
+            }
 
-                    @Override
-                    public void onFailure(ParticleCloudException exception) {
-                        Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
+            @Override
+            public void onFailure(ParticleCloudException exception) {
+                Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
