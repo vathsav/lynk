0fd9b2bd1cf3b19c3cb66026937c8ade51c8ac1f
vathsav <vathsav@live.in>
2016-08-17 12:38:45 +0530

    Refactor: Modified Core's function names in the .ino file.

--- core/lynk.ino
+++ core/lynk.ino
@@ -1,142 +1,122 @@
 /* Function prototypes -------------------------------------------------------*/
-int tinkerDigitalRead(String pin);
-int tinkerDigitalWrite(String command);
-int tinkerAnalogRead(String pin);
-int tinkerAnalogWrite(String command);
+int lynkDigitalRead(String pin);
+int lynkDigitalWrite(String command);
+int lynkAnalogRead(String pin);
+int lynkAnalogWrite(String command);
 
-/* This function is called once at start up ----------------------------------*/
-void setup()
-{
-	//Setup the Tinker application here
-
-	//Register all the Tinker functions
-	Spark.function("digitalRead", tinkerDigitalRead);
-	Spark.function("digitalWrite", tinkerDigitalWrite);
-	Spark.function("analogRead", tinkerAnalogRead);
-	Spark.function("analogWrite", tinkerAnalogWrite);
-
+void setup() {
+	//Register all the Lynk functions
+	Spark.function("digitalRead", lynkDigitalRead);
+	Spark.function("digitalWrite", lynkDigitalWrite);
+	Spark.function("analogRead", lynkAnalogRead);
+	Spark.function("analogWrite", lynkAnalogWrite);
+	Spark.function("supermassiveEmp", lynkBlow);
 }
 
-/* This function loops forever --------------------------------------------*/
-void loop()
-{
-	//This will run in a loop
-}
+void loop() {}
 
 /*******************************************************************************
- * Function Name  : tinkerDigitalRead
+ * Function Name  : lynkDigitalRead
  * Description    : Reads the digital value of a given pin
  * Input          : Pin
  * Output         : None.
  * Return         : Value of the pin (0 or 1) in INT type
                     Returns a negative number on failure
  *******************************************************************************/
-int tinkerDigitalRead(String pin)
-{
+int lynkDigitalRead(String pin) {
 	//convert ascii to integer
 	int pinNumber = pin.charAt(1) - '0';
 	//Sanity check to see if the pin numbers are within limits
-	if (pinNumber< 0 || pinNumber >7) return -1;
+	if (pinNumber < 0 || pinNumber > 7) return -1;
 
-	if(pin.startsWith("D"))
-	{
+	if(pin.startsWith("D")) {
 		pinMode(pinNumber, INPUT_PULLDOWN);
 		return digitalRead(pinNumber);
 	}
-	else if (pin.startsWith("A"))
-	{
-		pinMode(pinNumber+10, INPUT_PULLDOWN);
-		return digitalRead(pinNumber+10);
+	else if (pin.startsWith("A")) {
+		pinMode(pinNumber + 10, INPUT_PULLDOWN);
+		return digitalRead(pinNumber + 10);
 	}
 	return -2;
 }
 
 /*******************************************************************************
- * Function Name  : tinkerDigitalWrite
+ * Function Name  : lynkDigitalWrite
  * Description    : Sets the specified pin HIGH or LOW
  * Input          : Pin and value
  * Output         : None.
  * Return         : 1 on success and a negative number on failure
  *******************************************************************************/
-int tinkerDigitalWrite(String command)
-{
+int lynkDigitalWrite(String command) {
 	bool value = 0;
 	//convert ascii to integer
 	int pinNumber = command.charAt(1) - '0';
 	//Sanity check to see if the pin numbers are within limits
-	if (pinNumber< 0 || pinNumber >7) return -1;
+	if (pinNumber < 0 || pinNumber > 7) return -1;
 
 	if(command.substring(3) == "1") value = 1;
 	else if(command.substring(3) == "0") value = 0;
 	else return -2;
 
-	if(command.startsWith("D"))
-	{
+	if(command.startsWith("D")) {
 		pinMode(pinNumber, OUTPUT);
 		digitalWrite(pinNumber, value);
 		return 1;
 	}
-	else if(command.startsWith("A"))
-	{
-		pinMode(pinNumber+10, OUTPUT);
-		digitalWrite(pinNumber+10, value);
+	else if(command.startsWith("A")) {
+		pinMode(pinNumber + 10, OUTPUT);
+		digitalWrite(pinNumber + 10, value);
 		return 1;
 	}
 	else return -3;
 }
 
 /*******************************************************************************
- * Function Name  : tinkerAnalogRead
+ * Function Name  : lynkAnalogRead
  * Description    : Reads the analog value of a pin
  * Input          : Pin
  * Output         : None.
  * Return         : Returns the analog value in INT type (0 to 4095)
                     Returns a negative number on failure
  *******************************************************************************/
-int tinkerAnalogRead(String pin)
-{
+int lynkAnalogRead(String pin) {
 	//convert ascii to integer
 	int pinNumber = pin.charAt(1) - '0';
 	//Sanity check to see if the pin numbers are within limits
-	if (pinNumber< 0 || pinNumber >7) return -1;
+	if (pinNumber < 0 || pinNumber > 7) return -1;
 
-	if(pin.startsWith("D"))
-	{
+	if(pin.startsWith("D")) {
 		return -3;
 	}   
-	else if (pin.startsWith("A"))
-	{
-		return analogRead(pinNumber+10);
+	else if (pin.startsWith("A")) {
+		return analogRead(pinNumber + 10);
 	}
 	return -2;
 }
 
 /*******************************************************************************
- * Function Name  : tinkerAnalogWrite
+ * Function Name  : lynkAnalogWrite
  * Description    : Writes an analog value (PWM) to the specified pin
  * Input          : Pin and Value (0 to 255)
  * Output         : None.
  * Return         : 1 on success and a negative number on failure
  *******************************************************************************/
-int tinkerAnalogWrite(String command)
-{
+int lynkAnalogWrite(String command) {
 	//convert ascii to integer
 	int pinNumber = command.charAt(1) - '0';
 	//Sanity check to see if the pin numbers are within limits
-	if (pinNumber< 0 || pinNumber >7) return -1;
+	if (pinNumber < 0 || pinNumber > 7) return -1;
 
 	String value = command.substring(3);
 
-	if(command.startsWith("D"))
-	{
+	if(command.startsWith("D")) {
 		pinMode(pinNumber, OUTPUT);
 		analogWrite(pinNumber, value.toInt());
 		return 1;
 	}
-	else if(command.startsWith("A"))
-	{
-		pinMode(pinNumber+10, OUTPUT);
-		analogWrite(pinNumber+10, value.toInt());
+	else if(command.startsWith("A")) {
+		pinMode(pinNumber + 10, OUTPUT);
+		analogWrite(pinNumber + 10, value.toInt());
 		return 1;
 	}
 	else return -2;
