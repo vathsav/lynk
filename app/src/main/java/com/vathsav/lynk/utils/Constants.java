1909aafcb6d665a77258e787b0d42ab4d523be22
vathsav <vathsav@live.in>
2016-08-23 15:10:25 +0530

    refactor: Handled declaration redundancies using the AS Inspector.

--- app/src/main/java/com/vathsav/lynk/utils/Constants.java
+++ app/src/main/java/com/vathsav/lynk/utils/Constants.java
@@ -9,57 +9,57 @@ import com.google.firebase.database.FirebaseDatabase;
 public class Constants {
 
     // Intents
-    public static String intentHome = "com.vathsav.lynk.MAIN";
+    public static final String intentHome = "com.vathsav.lynk.MAIN";
 
     // Firebase references
-    private static FirebaseDatabase database = FirebaseDatabase.getInstance();
-    public static DatabaseReference globalsReference = database.getReference("globals");
-    public static DatabaseReference peripheralsReference = database.getReference("peripherals");
+    private static final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    public static final DatabaseReference globalsReference = database.getReference("globals");
+    public static final DatabaseReference peripheralsReference = database.getReference("peripherals");
 
     // Names of Cores
-    public static String particleRuthlessDynamite = "ruthless_dynamite";
+    public static final String particleRuthlessDynamite = "ruthless_dynamite";
 
     // Firebase nodes
-    public static String lynkGlobalsServerTimestamp = "server_timestamp";
+    public static final String lynkGlobalsServerTimestamp = "server_timestamp";
 
     // Toasts
-    public static String toastCommandCancelled = "Command cancelled";
-    public static String toastCoreOffline = "Core is offline.";
-    public static String toastIncorrectFunction = "Function doesn't exist on the Core!";
-    public static String toastUnableToFindCore = "Unable to find Core.";
-    public static String toastInvalidLoginCredentials = "Invalid login credentials.";
+    public static final String toastCommandCancelled = "Command cancelled";
+    public static final String toastCoreOffline = "Core is offline.";
+    public static final String toastIncorrectFunction = "Function doesn't exist on the Core!";
+    public static final String toastUnableToFindCore = "Unable to find Core.";
+    public static final String toastInvalidLoginCredentials = "Invalid login credentials.";
 
     // Peripheral names
-    public static String peripheralLight = "light";
-    public static String peripheralFan = "fan";
-//    public static String peripheralCommonOne = "something";
-//    public static String peripheralCommonTwo = "something";
+    public static final String peripheralLight = "light";
+    public static final String peripheralFan = "fan";
+//    public static final String peripheralCommonOne = "something";
+//    public static final String peripheralCommonTwo = "something";
 
     // Functions on the Core
-    public static String digitalWrite = "digitalWrite";
-    public static String digitalRead = "digitalRead";
-    public static String analogWrite = "analogWrite";
-    public static String analogRead = "analogRead";
-    public static String fireEmp = "skadoosh";
+    public static final String digitalWrite = "digitalWrite";
+    public static final String digitalRead = "digitalRead";
+    public static final String analogWrite = "analogWrite";
+    public static final String analogRead = "analogRead";
+    public static final String fireEmp = "skadoosh";
 
     // Dedicated GPIO configuration
-    public static String userOneLight = "D0";
-    public static String userOneFan = "A0";
-    public static String userTwoLight = "D2";
-    public static String userTwoFan = "A2";
-    public static String userThreeLight = "D4";
-    public static String userThreeFan = "A4";
-    public static String userFourLight = "D6";
-    public static String userFourFan = "A6";
+    public static final String userOneLight = "D0";
+    public static final String userOneFan = "A0";
+    public static final String userTwoLight = "D2";
+    public static final String userTwoFan = "A2";
+    public static final String userThreeLight = "D4";
+    public static final String userThreeFan = "A4";
+    public static final String userFourLight = "D6";
+    public static final String userFourFan = "A6";
 //    public static String commonOne = "A";
 //    public static String commonTwo = "A";
 
     // EMP code :P
-    public static String skadooshCode = "pandasareawesome";
+    public static final String skadooshCode = "pandasareawesome";
 
     // Names of users
-    public static String userOne = "Sentinel";
-    public static String userTwo = "El Diablo";
-    public static String userThree = "Deadshot";
-    public static String userFour = "Crazy Ivan";
+    public static final String userOne = "Sentinel";
+    public static final String userTwo = "El Diablo";
+    public static final String userThree = "Deadshot";
+    public static final String userFour = "Crazy Ivan";
 }
